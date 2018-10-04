6abf4d9930fa5297b75c8650209317e88f40c304
vu.pt <vu.pt@temando.com>
2018-10-04 17:29:20 +0700

    Commit 1st working

---
+++ README.md
@@ -0,0 +1,10 @@
+# amz-vipon-node-js
+
+## Introduction
+
+A tool which crawl vipon then get amz products info
+
+## Installation
+
+1. yarn install (Yarn is alternatives of npm)
+2. node index.js
--- index.js
+++ index.js
@@ -1,43 +1,97 @@
 var cheerio = require('cheerio');
-var request = require('request');
+var request = require('request-promise');
+var jsonParser = require('json-parser');
 
-async function getViponURL() {
-  // Get data from Vipon first page then push links into array
-  var url = 'https://www.vipon.com';
+// Get JSON page content for category pages of vipon
+const getJSONPageContent = uri => {
+  const options = {
+    uri,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    transform: body => {
+      var jsonObject = jsonParser.parse(body);
+      return cheerio.load(jsonObject.html);
+    }
+  };
+
+  return request(options);
+};
+
+// Get HTML page content for product pages of vipon and amz
+const getHTMLPageContent = uri => {
+  const options = {
+    uri,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    transform: body => {
+      console.log(uri);
+      return cheerio.load(body);
+    }
+  };
+  return request(options);
+};
+
+// Get product link from vipon HTML page if possible
+const htmlToViponURL = $ => {
   var viponURLs = [];
-  request(url, function(err, resp, body) {
-    if (err) throw err;
-    $ = cheerio.load(body);
-    $('.title').each(function() {
-      //   console.log($(this).text());
-      const viponID = $(this)
-        .attr('onclick')
-        .replace('review_product(', '')
-        .replace(')', '');
-      console.log(`https://www.vipon.com/product/${viponID}`);
-      //   console.log('---');
-      viponURLs.push(`https://www.vipon.com/product/${viponID}`);
-    });
+  $('.title').each(function() {
+    const viponID = $(this)
+      .attr('onclick')
+      .replace('review_product(', '')
+      .replace(')', '');
+    viponURLs.push(`https://www.vipon.com/product/${viponID}`);
   });
+  return viponURLs;
+};
 
-  // Call each vipon to get amz url
-  console.log(viponURLs);
-  var amazonURLs = [];
-  viponURLs.forEach(
-    viponURL =>
-      function() {
-        console.log(viponURL);
-        request(viponURL, function(err, resp, body) {
-          if (err) throw err;
-          $ = cheerio.load(body);
-          const amzURL = $(
-            'ul.list-unstyled:nth-child(3) > li:nth-child(1) > a:nth-child(1)'
-          ).attr('href');
-          amazonURLs.push(amzURL);
+// Get amz product link from vipon HTML page if possible
+const htmlToAmazonURL = $ => {
+  const amazonURL = $(
+    'ul.list-unstyled:nth-child(3) > li:nth-child(1) > a:nth-child(1)'
+  ).attr('href');
+  return amazonURL;
+};
+
+// Get title
+const titleAMZ = $ => {
+  const result = $('#productTitle').text();
+  return result;
+};
+
+// Run normal
+async function index() {
+  var all = [];
+  var i = 1;
+  var isEnd = false;
+  while (!isEnd) {
+    await getJSONPageContent(
+      //TODO: change 1 to ${i};
+      `https://www.vipon.com/promotion/search?domain=www.amazon.com&page=1`
+    )
+      .then($ => htmlToViponURL($))
+      .then(viponURLs => {
+        if (viponURLs.length !== 0) {
+          all.push(...viponURLs);
+          i++;
+          //TODO: Remove isEnd here
+          isEnd = true;
+        } else {
+          isEnd = true;
+        }
+      });
+  }
+
+  await getHTMLPageContent(all[0])
+    .then($ => htmlToAmazonURL($))
+    .then(url => {
+      getHTMLPageContent(url)
+        .then($ => titleAMZ($))
+        .then(result => {
+          console.log(result);
         });
-      }
-  );
-  console.log(amazonURLs);
+    });
 }
 
-getViponURL();
+index();
